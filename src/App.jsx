e00a8fa6540ff0a2e88f9ebda8e8257c292c97cf
DavidShale <olufemiwonder78@gmail.com>
2026-03-27 10:19:29 -0700

    nw

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,40 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Layout from './components/templates/Layout'
-import LandingPage from './pages/LandingPage'
-// Assuming these pages will be created soon
-import Dashboard from './pages/Dashboard'
-import Accounts from './pages/Accounts'
-import Groups from './pages/Groups'
-import Activity from './pages/Activity'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-import DashboardLayout from './components/templates/DashboardLayout'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
-import CreateGroup from './pages/CreateGroup'
-import JoinGroup from './pages/JoinGroup'
+// Layouts
+import Navbar from './components/organisms/Navbar';
+import DashboardLayout from './components/templates/DashboardLayout';
+
+// Pages
+import LandingPage from './pages/LandingPage';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Dashboard from './pages/Dashboard';
+import Activity from './pages/Activity';
+import Accounts from './pages/Accounts';
+import Groups from './pages/Groups';
+import CreateGroup from './pages/CreateGroup';
+import JoinGroup from './pages/JoinGroup';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<LandingPage />} />
-      </Route>
+      {/* Public Routes */}
+      <Route path="/" element={<><Navbar /><LandingPage /></>} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+
+      {/* Dashboard Routes (Protected Layout) */}
       <Route element={<DashboardLayout />}>
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/activity" element={<Activity />} />
         <Route path="/accounts" element={<Accounts />} />
         <Route path="/groups" element={<Groups />} />
         <Route path="/groups/create" element={<CreateGroup />} />
         <Route path="/groups/join" element={<JoinGroup />} />
-        <Route path="/activity" element={<Activity />} />
       </Route>
     </Routes>
   );
 }
 
-export default App
+export default App;
